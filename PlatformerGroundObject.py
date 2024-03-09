9b0854ec69696d649b32b0788eb47b9eca49fd5a
AndrewLHellman <142917243+AndrewLHellman@users.noreply.github.com>
2024-03-09 11:55:42 -0600

    Fix PlatformerGroundObject pos and rendering

--- PlatformerGroundObject.py
+++ PlatformerGroundObject.py
@@ -4,7 +4,7 @@ from Vector3 import Vector3
 
 class PlatformerGroundObject(MapObject):
   def __init__(self):
-    self.pos = Vector3(-5000,0,800)
+    self.pos = Vector3(-5000,0,-800)
     self.width = 10000
     self.depth = 0
     self.height = 500
@@ -17,7 +17,7 @@ class PlatformerGroundObject(MapObject):
 
   def render(self, screen, camera_view, p_pos):
     if camera_view == 'side':
-      render_pos = [self.pos.x - p_pos.x + screen.get_width()/2, self.pos.z, self.width, self.height]
+      render_pos = [self.pos.x - p_pos.x + screen.get_width()/2, -self.pos.z, self.width, self.height]
       # pygame.draw.rect(screen, (0, 0, 255), [self.pos.x - p_pos.x, self.pos.z, self.width, self.height])
       screen.blit(self.side_surf, render_pos)
       print(self.pos.x, self.pos.z)
