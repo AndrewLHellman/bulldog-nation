93d3dd30f43d4d25a262cd35ec0bc3d019b7a6c8
Zack Murry <zackk.murryy@gmail.com>
2024-03-09 11:45:47 -0600

    Add ground to platformer; fix thrust

--- Map.py
+++ Map.py
@@ -1,10 +1,12 @@
 from BoxObject import BoxObject
 from Vector3 import Vector3
+from PlatformerGroundObject import PlatformerGroundObject
 
 class Map:
   def __init__(self):
     self.objects = []
     self.objects.append(BoxObject(Vector3(-200, 200, 200), 100, 75, 50))
+    self.objects.append(PlatformerGroundObject())
 
   def render(self, screen, camera_view, p_pos):
     for object in self.objects:
