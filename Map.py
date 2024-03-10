150a459cb91701417e7c1651486d5545a140c6db
AndrewLHellman <142917243+AndrewLHellman@users.noreply.github.com>
2024-03-10 13:36:19 -0500

    Add new specimens and cloud obstacle

--- Map.py
+++ Map.py
@@ -11,9 +11,12 @@ class Map:
       for j in range(-700, 700, 250):
         self.objects.append(PlatformerGroundObject(i, j, min(randint(1, 5), 3)) if i != 750 else PlatformerGroundObject(750, j, randint(4, 5), 1))
     self.objects.append(BoxObject(Vector3(100, 100, 0), Vector3(100, 200, 50), (0, 0, 255), True))
-    self.objects.append(BoxObject(Vector3(837.5, -700, 150), Vector3(75, 1400, 50), (255, 255, 255, 0), False, transparent=True))
+    self.objects.append(BoxObject(Vector3(837.5, -700, 198), Vector3(75, 1400, 1), (255, 255, 255, 0), False, transparent=True))
     self.objects.append(BoxObject(Vector3(1300, -400, 130), Vector3(75, 75, 75), collectable=True, top_image='./source/sprites/SpecimenTop.png', side_image='./source/sprites/SpecimenSide.png'))
     self.objects.append(BoxObject(Vector3(2100, -150, -490), Vector3(300, 300, 700), top_image='./source/sprites/RockObstacleTop.png', side_image='./source/sprites/RockObstacleSide.png'))
+    self.objects.append(BoxObject(Vector3(2230, -37.5, -515), Vector3(75, 75, 75), collectable=True, top_image='./source/sprites/SpecimenTop.png', side_image='./source/sprites/SpecimenSide.png'))
+    self.objects.append(BoxObject(Vector3(3912.5, 37.5, 130), Vector3(75, 75, 75), collectable=True, top_image='./source/sprites/SpecimenTop.png', side_image='./source/sprites/SpecimenSide.png'))
+    self.objects.append(BoxObject(Vector3(3600, -350, -600), Vector3(700, 700, 600), top_image='./source/sprites/CloudObstacleTop.png', side_image='./source/sprites/CloudObstacleSide.png'))
 
   def render(self, screen, camera_view, p_pos):
     for object in self.objects:
